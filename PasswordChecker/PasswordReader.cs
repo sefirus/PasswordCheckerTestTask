0b100024933a277e1cfae2bc243b2e0d79174d22
sefirus <sashasam348@gmail.com>
2023-06-25 16:49:37 +0300

    Basically implemented reader

--- PasswordChecker/PasswordReader.cs
+++ PasswordChecker/PasswordReader.cs
@@ -7,6 +7,40 @@ public class PasswordReader : IPasswordReader
 {
     public IEnumerable<PasswordEntry> ReadPasswords(string filename)
     {
-        throw new NotImplementedException();
+        var lines = File.ReadLines(filename);
+
+        foreach (var line in lines)
+        {
+            yield return ParseLine(line);
+        }
+    }
+
+    private static PasswordEntry ParseLine(ReadOnlySpan<char> line)
+    {
+        var index1 = line.IndexOf(' ');
+        var index2 = line.IndexOf('-');
+        var index3 = line.IndexOf(':');
+
+        if (!int.TryParse(line.Slice(index1 + 1, index2 - index1 - 1), out var min)
+            || !int.TryParse(line.Slice(index2 + 1, index3 - index2 - 2), out var max))
+        {
+            throw new InvalidOperationException();
+        }
+            
+        var requiredChar = line[0];
+        var password = line[(index3 + 2)..]; // Still need to convert to Memory<char> to use Trim()
+
+        var rule = new ValidationRule
+        {
+            RequiredChar = requiredChar,
+            Min = min,
+            Max = max
+        };
+
+        return new PasswordEntry
+        {
+            Rule = rule,
+            Password = password.ToArray()
+        };
     }
 }
