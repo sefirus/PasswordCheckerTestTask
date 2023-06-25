de6b9747b124dfcd4fa832c415f7c85e22fe0997
sefirus <sashasam348@gmail.com>
2023-06-25 16:25:04 +0300

    Implemented validator

--- PasswordChecker/PasswordValidator.cs
+++ PasswordChecker/PasswordValidator.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.HighPerformance;
 using PasswordChecker.Interfaces;
 using PasswordChecker.Models;
 
@@ -7,6 +8,8 @@ public class PasswordValidator : IPasswordValidator
 {
     public bool IsValid(PasswordEntry passwordEntry)
     {
-        throw new NotImplementedException();
+        var charCount = passwordEntry.Password.Span.Count(passwordEntry.Rule.RequiredChar);
+        var isValid = charCount >= passwordEntry.Rule.Min && charCount <= passwordEntry.Rule.Max;
+        return isValid;
     }
 }
